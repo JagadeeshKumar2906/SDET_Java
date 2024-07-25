4536a424e1caccb16aea8b8596ddfc213eb26cc1
JagadeeshKumar2906 <jagadeeshmvjk@gmail.com>
2024-07-25 18:54:30 +0530

    Latest_Files

---
+++ src/Ex_210724/Exercise_003_OddorEven.java
@@ -0,0 +1,19 @@
+package Ex_210724;
+
+public class Exercise_003_OddorEven {
+    public static void main(String[] args) {
+        for (int i = 0; i <=50; i++) {
+            if(i % 2 ==0)
+            {
+                System.out.println("Even Number"+i);
+                //continue;
+            }
+            else
+            {
+                System.out.println("Odd Number"+i);
+            }
+
+
+        }
+    }
+}
