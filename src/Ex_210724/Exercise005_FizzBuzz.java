85d854a27a06461003e8a924bd733a9ce6ae6ee6
JagadeeshKumar2906 <jagadeeshmvjk@gmail.com>
2024-07-26 19:33:26 +0530

    Latest_Files

---
+++ src/Ex_210724/Exercise005_FizzBuzz.java
@@ -0,0 +1,26 @@
+package Ex_210724;
+
+//import java.util.Scanner;
+
+public class Exercise005_FizzBuzz {
+    public static void main(String[] args) {
+        for (int i = 1; i <= 500; i++) {
+            if (i % 3 == 0 && i % 5 == 0)
+            {
+                System.out.println("FizzBuzz");
+            }
+            else if (i % 3 == 0)
+            {
+                System.out.println("Fizz");
+            }
+           else if (i % 5 == 0)
+           {
+                System.out.println("Buzz");
+            }
+            else
+            {
+                System.out.println(i);
+            }
+        }
+    }
+    }
