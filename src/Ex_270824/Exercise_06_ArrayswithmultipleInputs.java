c150c53ee0929c697c9ab684de1abbfb1637d45f
JagadeeshKumar2906 <jagadeeshmvjk@gmail.com>
2024-07-31 17:39:25 +0530

    Latest_Files

---
+++ src/Ex_270824/Exercise_06_ArrayswithmultipleInputs.java
@@ -0,0 +1,24 @@
+package Ex_270824;
+
+import java.util.Scanner;
+
+public class Exercise_06_ArrayswithmultipleInputs {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int[] a = new int[5];
+        for (int i = 0; i <a.length; i++) {
+           System.out.println("Enter your Marks:" + (i + 1));
+            a[i] = sc.nextInt();
+       }
+        for (int i = 0; i <a.length; i++) {
+            System.out.println("The Entered Marks are:" + (i + 1)+a[i]);
+        }
+           // int j=0;
+          //  while (j<a.length)
+         //   {
+         //       System.out.println(a[j]);
+         //       j++;
+          //  }
+        }
+    }
+
