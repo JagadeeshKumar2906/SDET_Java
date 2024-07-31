86cfbe08c2be06643c35ba4badb98a45510cc173
JagadeeshKumar2906 <jagadeeshmvjk@gmail.com>
2024-07-31 13:00:01 +0530

    Latest_Files

---
+++ src/Ex_270824/Exercise_03_ArraysWithScanner.java
@@ -0,0 +1,21 @@
+package Ex_270824;
+
+import java.util.Scanner;
+
+public class Exercise_03_ArraysWithScanner {
+    public static void main(String[] args) {
+        int [] marks = new int[5];
+        System.out.println("Enter the 1st mark:");
+        Scanner sc = new Scanner(System.in);
+        marks[0]= sc.nextInt();
+        marks[1]=99;
+        marks[2]=90;
+        marks[3]=80;
+        System.out.println("Enterd mark is: "+marks[0]);
+        System.out.println("2nd mark:"+marks[1]);
+        System.out.println("3rd mark:"+marks[2]);
+        System.out.println("4th mark:"+marks[3]);
+
+
+    }
+}
