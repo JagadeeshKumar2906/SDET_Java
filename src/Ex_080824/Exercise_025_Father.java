2777397e2fe9a2ccab99974c7895dbe3a3f4b4c9
JagadeeshKumar2906 <jagadeeshmvjk@gmail.com>
2024-08-14 13:10:35 +0530

    Latest_Files

---
+++ src/Ex_080824/Exercise_025_Father.java
@@ -0,0 +1,11 @@
+package Ex_080824;
+
+public class Exercise_025_Father
+{
+    void fathercarmodel()
+    {
+        String name;
+        int price;
+    }
+
+}
---
+++ src/Ex_080824/Exercise_026_Son.java
@@ -0,0 +1,27 @@
+package Ex_080824;
+
+public class Exercise_026_Son extends Exercise_025_Father
+{
+    String name;
+    int price;
+
+    void soncarmodel(String nname, int pprice)
+    {
+        this.name = nname;
+        this.price = pprice;
+    }
+    public static void main(String[] args)
+    {
+        Exercise_026_Son s1 = new Exercise_026_Son();
+        s1.soncarmodel("BMW",990000);
+        {
+            System.out.println("Son Car Name is:"+s1.name);
+            System.out.println("Price of the Car is:"+s1.price);
+        }
+        s1.fathercarmodel();
+        {
+            System.out.println("Father Car is Maruthi");
+            System.out.println("Price is : 1000000");
+        }
+    }
+}
